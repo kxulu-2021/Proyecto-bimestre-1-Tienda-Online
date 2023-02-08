747a5f67acd53e9a0d22b67a8c7b1b06446ea042
Kevin Xulú <kxulu-2021348@kinal.edu.gt>
2023-02-07 23:53:53 -0600

    Primer commit V1

---
+++ routes/categoria.js
@@ -0,0 +1,13 @@
+const { Router } = require('express');
+const { getCategoria, postCategoria, putCategoria, deleteCategoria } = require('../controllers/categoria');
+
+const router = Router();
+
+router.get('/mostrar', getCategoria)
+router.post('/agregar', postCategoria)
+router.put('/editar/:id', putCategoria)
+router.delete('/delete/:id', deleteCategoria)
+
+
+
+module.exports = router;
